e7a8f43308385ac7ebde4dfa6024dbc57301d7bb
SCASTELLANO6044 <sergiocastellanotrabajo@gmail.com>
2023-09-08 14:22:29 +0100

    Backend

---
+++ Backend/findaflatapi/src/main/java/com/findaflatapi/FindaflatapiApplication.java
@@ -0,0 +1,13 @@
+package com.findaflatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindaflatapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindaflatapiApplication.class, args);
+	}
+
+}
